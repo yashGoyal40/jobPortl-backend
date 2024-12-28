fe6758b7ab558dd3667e436643f8f1cb4ff071b0
yashGoyal40 <yashg4118@gmail.com>
2024-12-28 21:20:58 +0530

    complete

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,7 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure:true,
     sameSite: 'None',
   };
 
